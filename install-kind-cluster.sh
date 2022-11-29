c2b759b40b4f7b8bc3743cd0117f1ac2305384a1
Rajesh Dutta <rdutta@Rajeshs-MacBook-Pro.local>
2022-11-29 22:26:56 +0100

    updated

---
+++ install-kind-cluster.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+kind create cluster --config kind/kind-cluster-1.yaml --name $1
+
+clear
+# checking kubernetes status
+until kubectl get sa default 2>&1 > /dev/null; do
+  echo "Waiting for Kubernetes to start..."
+  sleep 1
+done
+echo " `date` >>>>> kubernetes started..."
+
+# verifying if coreDNS pods are up and running
+until kubectl -n kube-system describe deployments.apps coredns | grep "0 unavailable"; do
+  kubectl -n kube-system rollout restart deployment coredns
+  echo "Waiting for coredns pods to start.."
+  sleep 20
+done
+
+clear
+echo "`date` >>>>> coreDNS started..."
+echo "`date` >>>>> cluster created successfully..."
